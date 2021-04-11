c57699595232dac723693f1b7631cf582e8b37c2
Romeo Orsolino <rorsolino@robots.ox.ac.uk>
2021-04-11 20:09:18 +0100

    mario rotates a motor

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -20,13 +20,39 @@
  */
 
 #include <stdio.h>
+#include "periph/gpio.h"
+#include "xtimer.h"
+#include "board.h"
+#include "thread.h"
+
+static char stack[THREAD_STACKSIZE_MAIN];
+
+gpio_t pin;
+gpio_t pin_dir;
+
+
+static void *thread_handler(void *arg){
+    (void)arg;
+    pin = GPIO_PIN(1,7);
+    pin_dir = GPIO_PIN(1,6);
+    gpio_init(pin, GPIO_OUT);
+    gpio_init(pin_dir, GPIO_OUT);
+    gpio_clear(pin_dir);
+    while(1){
+	gpio_toggle(pin);
+        //xtimer_usleep(20);
+	xtimer_ticks32_t last_wakeup = xtimer_now();
+	xtimer_periodic_wakeup(&last_wakeup, 30);
+    }
+    return NULL;
+}
 
 int main(void)
 {
     puts("Hello World!");
 
-    printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
-    printf("This board features a(n) %s MCU.\n", RIOT_MCU);
-
+    thread_create(stack, sizeof(stack), THREAD_PRIORITY_MAIN - 1,
+               0, thread_handler, NULL, "new thread");
+    while(1);
     return 0;
 }
